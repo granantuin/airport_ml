8c41e16203ef4f6366e3db7c5a008775f4d25028
granantuin <49106818+granantuin@users.noreply.github.com>
2023-06-18 12:19:45 +0200

    Update airport_ml.py

--- airport_ml.py
+++ airport_ml.py
@@ -178,6 +178,79 @@ def get_meteogalicia_model_4Km(coorde):
 
     return dffinal , control
 
+def get_meteogalicia_model_12Km(coorde):
+    """
+    get meteogalicia model (12Km)from algo coordenates
+    Returns
+    -------
+    dataframe with meteeorological variables forecasted.
+    """
+    
+    #defining url to get model from Meteogalicia server
+    var1 = "var=dir&var=mod&var=wind_gust&var=mslp&var=temp&var=rh&var=visibility&var=lhflx"
+    var2 = "&var=lwflx&var=conv_prec&var=prec&var=swflx&var=shflx&var=cape&var=cin&var=cfh&var=T850"
+    var3 = "&var=cfl&var=cfm&var=cft&var=HGT500&var=HGT850&var=T500&var=snow_prec&var=snowlevel"
+    var = var1+var2+var3 
+    head1="https://mandeo.meteogalicia.es/thredds/ncss/modelos/WRF_HIST/d02"
+    
+    
+    #url12="http://mandeo.meteogalicia.es/thredds/ncss/modelos/WRF_HIST/d02/2016/09/wrf_arw_det_history_d02_20160927_0000.nc4?var=mod&disableLLSubset=on&dis
+    try:
+          
+      today = pd.to_datetime("today")    
+      head2 = today.strftime("/%Y/%m/wrf_arw_det_history_d02")
+      head3 = today.strftime("_%Y%m%d_0000.nc4?")
+      head = head1+head2+head3
+       
+      f_day=(today+timedelta(days=2)).strftime("%Y-%m-%d") 
+      tail="&time_start="+today.strftime("%Y-%m-%d")+"T01%3A00%3A00Z&time_end="+f_day+"T23%3A00%3A00Z&accept=csv"
+  
+      dffinal=pd.DataFrame() 
+      for coor in list(zip(coorde.lat.tolist(),coorde.lon.tolist(),np.arange(0,len(coorde.lat.tolist())).astype(str))):
+          dffinal=pd.concat([dffinal,pd.read_csv(head+var+"&latitude="+str(coor[0])+"&longitude="+str(coor[1])+tail,).add_suffix(str(coor[2]))],axis=1)    
+  
+      #filter all columns with lat lon and date
+      dffinal=dffinal.filter(regex='^(?!(lat|lon|date).*?)')
+  
+      #remove column string between brakets
+      new_col=[c.split("[")[0]+c.split("]")[-1] for c in dffinal.columns]
+      for col in zip(dffinal.columns,new_col):
+          dffinal=dffinal.rename(columns = {col[0]:col[1]})
+  
+      dffinal=dffinal.set_index(pd.date_range(start=today.strftime("%Y-%m-%d"), end=(today+timedelta(days=3)).strftime("%Y-%m-%d"), freq="H")[1:-1])  
+      control = True
+          
+    except:
+
+      today = pd.to_datetime("today")-timedelta(1)
+      head2 = today.strftime("/%Y/%m/wrf_arw_det_history_d02")
+      head3 = today.strftime("_%Y%m%d_0000.nc4?")
+      head = head1+head2+head3
+        
+      f_day=(today+timedelta(days=2)).strftime("%Y-%m-%d") 
+      tail="&time_start="+today.strftime("%Y-%m-%d")+"T01%3A00%3A00Z&time_end="+f_day+"T23%3A00%3A00Z&accept=csv"
+  
+      dffinal=pd.DataFrame() 
+      for coor in list(zip(coorde.lat.tolist(),coorde.lon.tolist(),np.arange(0,len(coorde.lat.tolist())).astype(str))):
+          dffinal=pd.concat([dffinal,pd.read_csv(head+var+"&latitude="+str(coor[0])+"&longitude="+str(coor[1])+tail,).add_suffix(str(coor[2]))],axis=1)    
+  
+      
+      #filter all columns with lat lon and date
+      dffinal=dffinal.filter(regex='^(?!(lat|lon|date).*?)')
+  
+      #remove column string between brakets
+      new_col=[c.split("[")[0]+c.split("]")[-1] for c in dffinal.columns]
+      for col in zip(dffinal.columns,new_col):
+          dffinal=dffinal.rename(columns = {col[0]:col[1]})
+  
+      dffinal=dffinal.set_index(pd.date_range(start=today.strftime("%Y-%m-%d"), end=(today+timedelta(days=3)).strftime("%Y-%m-%d"), freq="H")[1:-1])  
+      control= False  
+
+     
+    return dffinal , control
+
+
+
 
 options = ["LECO", "LEST", "LEBL"]
 default_option = options[0]  # Set the default option
@@ -197,8 +270,15 @@ best_wrf = []
 algo_dir = OACI+"/algorithms/"
 
 #get meteorological model from algorithm file. Select "coor" key to get coordinates. Pick up first algorithm all same coordinates
-meteo_model,con = get_meteogalicia_model_4Km(pickle.load(open(algo_dir+os.listdir(algo_dir)[0],"rb"))["coor"])
 
+#grid type
+4k = ["LECO","LEST"]
+12k = ["LEBL"]
+
+if OACI in 4k:
+    meteo_model,con = get_meteogalicia_model_4Km(pickle.load(open(algo_dir+os.listdir(algo_dir)[0],"rb"))["coor"])
+else:
+    meteo_model,con = get_meteogalicia_model_12Km(pickle.load(open(algo_dir+os.listdir(algo_dir)[0],"rb"))["coor"])
 
 
 #add time variables
